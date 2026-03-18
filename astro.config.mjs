ea0f197d664996c5e3fd3e7fbc411822afcd1add
seyren10 <gagarinroyvictor@gmail.com>
2026-03-18 16:44:14 +0800

    feat: add new components and assets for portfolio site
    - Created About, Contact, Footer, Header, Hero, Projects, and Skills components.
    - Added SVG assets for TypeScript and Vue.js.
    - Updated main layout to include new sections.
    - Enhanced styling in global CSS for improved aesthetics.
    - Implemented AppBadge component for displaying technology icons.
    - Adjusted header positioning for better UX.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 
+import lenis from "astro-lenis";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), lenis()],
 })
